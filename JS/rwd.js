d85f93e27b29d47f12b5ef453a91f405bc42eaed
XUEHONGTANG <ss910605as@gmail.com>
2022-07-29 23:39:37 +0800

    rwd

--- JS/rwd.js
+++ JS/rwd.js
@@ -10,5 +10,17 @@ const nav = document.querySelector("section.nav");
     nav.classList.toggle("nav_show")
 });
 
+window.addEventListener("resize", function(){
+    let swiper_slide_img = document.querySelectorAll("img.home_top_img");
+    if(window.innerWidth <= 414){
+        swiper_slide_img[0].setAttribute("src", "../img/rwd1.jpg");
+        swiper_slide_img[1].setAttribute("src", "../img/rwd2.jpg");
+        swiper_slide_img[2].setAttribute("src", "../img/rwd3.jpg");
+    } else {
+        swiper_slide_img[0].setAttribute("src", "../img/1.jpg");
+        swiper_slide_img[1].setAttribute("src", "../img/2.jpg");
+        swiper_slide_img[2].setAttribute("src", "../img/3.jpg");
+    }
+})
 
 
